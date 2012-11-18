b3956f6496f5dae44084e271148a55bda5e53839
Levin Alexander <mail@levinalex.net>
2012-11-18 21:03:56 +0100

    able to parse sections

--- lib/law/law.rb
+++ lib/law/law.rb
@@ -1,22 +1,46 @@
 class Law::Law
-  attr_accessor :id
+  attr_accessor :uid
   attr_accessor :short_name
   attr_accessor :title
 
-  def initialize
+  def initialize_from_xml(xml)
+    @sections = []
+
+    doc_meta = xml.at_xpath(%Q(dokumente/norm[/dokumente/@doknr=@doknr]/metadaten))
+
+    self.uid = doc_meta.at_xpath(%Q(./jurabk)).text
+    self.short_name = doc_meta.xpath(%Q(string(./kurzue)))
+
+    xml.xpath(%Q(dokumente/norm[/dokumente/@doknr!=@doknr])).each do |norm|
+      if norm.at_xpath(%Q(./metadaten/gliederungseinheit))
+        sn = norm.xpath("string(./metadaten/gliederungseinheit/gliederungskennzahl)")
+        bez = norm.xpath("string(./metadaten/gliederungseinheit/gliederungsbez)")
+        title = norm.xpath("string(./metadaten/gliederungseinheit/gliederungstitel)")
+
+        self.add_section(sn, bez, title)
+      end
+    end
+  end
+
+  def add_section(sid, bez, txt)
+    txt = txt.gsub(/\s+/, " ")
+    @sections << [sid, bez, txt]
   end
 
 
   def inspect
-    "law: #{id}"
+    s = "law: #{uid} - #{short_name}\n" +
+      @sections.map { |s| "\t" + s.join(" - ") }.join("\n")
 
+      s.strip
   end
 
   def self.from_xml(xml)
     law = allocate
-    law.id = xml.xpath("//dokumente/norm/metadaten/jurabk").first.text.strip
+    law.initialize_from_xml(xml)
 
     law
+
   end
 
 end
