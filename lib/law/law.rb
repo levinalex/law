b715bc0c9253f3f136d0e4edfe6bcbdd85c276b4
Levin Alexander <mail@levinalex.net>
2012-11-19 01:03:44 +0100

    Treeish

--- lib/law/law.rb
+++ lib/law/law.rb
@@ -1,46 +1,93 @@
-class Law::Law
-  attr_accessor :uid
-  attr_accessor :short_name
-  attr_accessor :title
-
-  def initialize_from_xml(xml)
-    @sections = []
-
-    doc_meta = xml.at_xpath(%Q(dokumente/norm[/dokumente/@doknr=@doknr]/metadaten))
-
-    self.uid = doc_meta.at_xpath(%Q(./jurabk)).text
-    self.short_name = doc_meta.xpath(%Q(string(./kurzue)))
-
-    xml.xpath(%Q(dokumente/norm[/dokumente/@doknr!=@doknr])).each do |norm|
-      if norm.at_xpath(%Q(./metadaten/gliederungseinheit))
-        sn = norm.xpath("string(./metadaten/gliederungseinheit/gliederungskennzahl)")
-        bez = norm.xpath("string(./metadaten/gliederungseinheit/gliederungsbez)")
-        title = norm.xpath("string(./metadaten/gliederungseinheit/gliederungstitel)")
-
-        self.add_section(sn, bez, title)
+module Law
+  module Treeish
+    def children
+      @children ||= []
+    end
+    def append(n)
+      if children.empty? || children.last.depth >= n.depth
+        children << n
+      else
+        children.last.append(n)
       end
+    end
+
+    def ascii_tree
+      subtree = children.map do |c|
+        c.ascii_tree.lines.map { |l| "   " + l.rstrip }.join("\n")
+      end
+
+      ["`- " + inspect + " (#{depth})", *subtree].join("\n")
     end
   end
 
-  def add_section(sid, bez, txt)
-    txt = txt.gsub(/\s+/, " ")
-    @sections << [sid, bez, txt]
+  class Section
+    include Treeish
+    attr_reader :depth
+
+    def initialize(xml)
+      @sn = xml.xpath("string(./metadaten/gliederungseinheit/gliederungskennzahl)")
+      @bez = xml.xpath("string(./metadaten/gliederungseinheit/gliederungsbez)")
+      @title = xml.xpath("string(./metadaten/gliederungseinheit/gliederungstitel)")
+      @title = @title.gsub(/\s+/, " ")
+
+      @depth = @sn.length / 3
+      @sections = []
+    end
+
+    def inspect
+      "#{@bez} #{@sn}"
+    end
   end
 
-
-  def inspect
-    s = "law: #{uid} - #{short_name}\n" +
-      @sections.map { |s| "\t" + s.join(" - ") }.join("\n")
-
-      s.strip
+  class Para
+    def initialize(xml)
+      @bez = xml.xpath(%Q(string(./metadaten/enbez)))
+      @title = xml.xpath("string(./metadaten/titel)")
+    end
   end
 
-  def self.from_xml(xml)
-    law = allocate
-    law.initialize_from_xml(xml)
+  class Law
+    include Treeish
 
-    law
+    attr_accessor :uid
+    attr_accessor :short_name
+    attr_accessor :title
+
+    def short_name=(val)
+      @short_name = val.empty? ? nil : val
+    end
+
+    def depth
+      0
+    end
+
+    def inspect
+      [@uid, @short_name].compact.join(" - ")
+    end
+
+
+    def add_para(para)
+    end
+
+    def initialize(xml)
+      @children = []
+
+      doc_meta = xml.at_xpath(%Q(dokumente/norm[/dokumente/@doknr=@doknr]/metadaten))
+
+      self.uid = doc_meta.at_xpath(%Q(./jurabk)).text
+      self.short_name = doc_meta.xpath(%Q(string(./kurzue)))
+
+      xml.xpath(%Q(dokumente/norm[/dokumente/@doknr!=@doknr])).each do |norm|
+        if norm.at_xpath(%Q(./metadaten/gliederungseinheit))
+          self.append(Section.new(norm))
+        elsif norm.at_xpath("./metadaten/enbez")
+          self.add_para(Para.new(norm))
+        else
+          raise "unexpected thing in XML"
+        end
+      end
+    end
+
 
   end
-
 end
